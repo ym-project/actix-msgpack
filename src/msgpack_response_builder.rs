168999df1fc4ebdbbec3c11b09f8b7a5b0a9f622
ym-project <ym-project@protonmail.com>
2023-12-23 09:35:03 +0300

    feat(#28): ♻️ leave only one responder

--- src/msgpack_response_builder.rs
+++ src/msgpack_response_builder.rs
@@ -6,24 +6,12 @@ use mime::APPLICATION_MSGPACK;
 use serde::Serialize;
 
 pub trait MsgPackResponseBuilder {
-	/// Responder with compact representation
+	/// MessagePack Responder
 	fn msgpack<T: Serialize>(&mut self, value: T) -> HttpResponse;
-	/// Responder with field names
-	fn msgpack_named<T: Serialize>(&mut self, value: T) -> HttpResponse;
 }
 
 impl MsgPackResponseBuilder for HttpResponseBuilder {
 	fn msgpack<T: Serialize>(&mut self, value: T) -> HttpResponse {
-		match rmp_serde::to_vec(&value) {
-			Ok(body) => {
-				self.insert_header(ContentType(APPLICATION_MSGPACK));
-				self.body(Bytes::from(body))
-			},
-			Err(err) => HttpResponse::from_error(MsgPackError::Serialize(err)),
-		}
-	}
-
-	fn msgpack_named<T: Serialize>(&mut self, value: T) -> HttpResponse {
 		match rmp_serde::to_vec_named(&value) {
 			Ok(body) => {
 				self.insert_header(ContentType(APPLICATION_MSGPACK));
