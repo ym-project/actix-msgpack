037e2d3b16d29420ab6bfe9ef4de0166c9be3429
ym-project <ym-project@protonmail.com>
2024-06-23 15:50:50 +0300

    Revert last release
    This reverts commits:
    - 67071d94537d1e11b20149c5013a8cac90cfd6a4
    - bd5105cf3f0451a5c06087fc92b656a4092314b5
    - 5235f764e5483b1a782dc481266884b330c1640c

--- src/msgpack_config.rs
+++ src/msgpack_config.rs
@@ -1,13 +1,12 @@
 use super::DEFAULT_PAYLOAD_LIMIT;
 
-#[derive(Clone, Copy)]
 pub struct MsgPackConfig {
 	pub(crate) limit: usize,
 }
 
 impl MsgPackConfig {
 	/// Set maximum accepted payload size in bytes. The default limit is 256KiB.
-	pub fn limit(mut self, limit: usize) -> Self {
+	pub fn limit(&mut self, limit: usize) -> &mut Self {
 		self.limit = limit;
 		self
 	}
